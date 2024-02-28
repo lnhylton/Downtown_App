41f612b3e7a173a01076459ad247aff9204020a4
markush1022 <homannm@vt.edu>
2024-02-27 19:55:04 -0500

    MapView

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 import LocationTracker from './LocationTracker';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LocatiogitnTracker/>
+      <LocationTracker/>
       <StatusBar style="auto" />
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -18,4 +20,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '50%',
+  }
 });
